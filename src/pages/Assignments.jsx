cb324e56ebce95a41adc57d13361c02c8797e7eb
istiak19 <istiak15-14128@diu.edu.bd>
2024-12-23 12:34:12 +0600

    seventeenth commit

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -2,27 +2,76 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Loading from "../components/Loading/Loading";
 import Card from "../components/Card/Card";
+import { useState } from "react";
 
 const Assignments = () => {
+
+    const [filter, setFilter] = useState('')
+    const [search, setSearch] = useState('')
+
     const { data: assignments, isError, isLoading } = useQuery({
-        queryKey: ['assignments'], queryFn: async () => {
-            const { data } = await axios.get('http://localhost:5000/assignments')
+        queryKey: ['assignments', filter, search], queryFn: async () => {
+            const { data } = await axios.get(`http://localhost:5000/assignments?filter=${filter}&search=${search}`)
             return data
         }
     })
 
-    if (isLoading) {
-        return <Loading></Loading>
-    }
+    // if (isLoading) {
+    //     return <Loading></Loading>
+    // }
 
     if (isError) console.log(isError);
-    
+
+    const handleReset = () => {
+        setFilter('');
+        setSearch('');
+    }
+
     return (
         <div>
+            {/* Filters and Search */}
+            <div className="flex flex-col md:flex-row justify-center items-center gap-5 my-8">
+                <select
+                    name="level"
+                    id="level"
+                    onChange={(e) => setFilter(e.target.value)}
+                    className="border p-4 rounded-lg"
+                    value={filter}
+                >
+                    <option value="">Filter By Difficulty Level</option>
+                    <option value="Easy">Easy</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Hard">Hard</option>
+                </select>
+                {/* Search */}
+                <label className="input input-bordered flex items-center gap-2">
+                    <input type="text"
+                        onChange={(e) => setSearch(e.target.value)}
+                        name="search"
+                        className="grow"
+                        placeholder="Search"
+                        value={search}
+                    />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                            fillRule="evenodd"
+                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                            clipRule="evenodd" />
+                    </svg>
+                </label>
+                <div>
+                    <button onClick={handleReset} className="btn bg-[#3F9CFF] text-white font-medium px-8">All Reset</button>
+                </div>
+            </div>
+            {/* Assignments card */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 my-10">
-                {
-                    assignments.map(assignment => <Card key={assignment._id} assignment={assignment}></Card>)
-                }
+                {assignments?.map((assignment) => (
+                    <Card key={assignment._id} assignment={assignment} />
+                ))}
             </div>
         </div>
     );
