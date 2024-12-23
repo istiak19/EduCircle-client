a4a585de63a88b0928efaecc0854dbcc52844f75
istiak19 <istiak15-14128@diu.edu.bd>
2024-12-23 11:41:24 +0600

    sixteenth commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,32 +1,56 @@
 import { useEffect, useState } from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Typewriter } from "react-simple-typewriter";
 
 const Features = () => {
-    const [features, setFeatures] = useState([])
+    const [features, setFeatures] = useState([]);
 
     useEffect(() => {
         AOS.init({ duration: 1000 });
     }, []);
 
     useEffect(() => {
-        fetch('./features.json')
-            .then(res => res.json())
-            .then(data => setFeatures(data))
-    }, [])
+        fetch("./features.json")
+            .then((res) => res.json())
+            .then((data) => setFeatures(data));
+    }, []);
 
     return (
         <div className="w-11/12 mx-auto my-10">
-            <h2 className="text-2xl font-semibold text-center mb-6">Key Features</h2>
+            <div className="App">
+                <h1 className="text-3xl font-bold text-center mb-6">
+                    Key Features{" "}
+                    <span style={{ color: "#3F9CFF" }}>
+                        <Typewriter
+                            words={[
+                                "Collaborative Group Study",
+                                "Assignment Management",
+                                "Real-Time Notifications",
+                                "FAQ and Help",
+                                "User-Friendly Interface",
+                                "Performance Tracking !"
+                            ]}
+                            loop={5}
+                            cursor
+                            cursorStyle="_"
+                            typeSpeed={70}
+                            deleteSpeed={50}
+                            delaySpeed={1000}
+                        />
+                    </span>
+                </h1>
+            </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {features.map((feature, idx) => (
                     <div
                         key={idx}
-                        className="border border-[#3F9CFF]rounded-lg p-4 text-center shadow-md" data-aos="fade-right"
+                        className="border border-[#3F9CFF] rounded-lg p-4 text-center shadow-md"
+                        data-aos="fade-right"
                     >
                         <img
                             src={feature.icon}
-                            alt='Feature image'
+                            alt="Feature"
                             className="w-20 h-20 mx-auto mb-3 rounded-md"
                         />
                         <h3 className="text-lg font-bold mb-2">{feature.title}</h3>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     }
 
     return (
-        <div className="navbar bg-[#3F9CFF] px-14 py-5">
+        <div className="navbar bg-[#3F9CFF] lg:px-14 py-2 sm:py-5">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
