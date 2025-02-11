20debbb2efe7cb35ea18c97eefc87883198e8c23
istiak19 <istiak15-14128@diu.edu.bd>
2025-02-11 19:30:49 +0600

    error page update

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,33 +1,33 @@
-import { FaArrowLeftLong } from "react-icons/fa6";
-import error from '../assets/404error.png'
+import { FaArrowLeft } from "react-icons/fa6";
+import error from "../assets/error.jpg";
 import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <section className='bg-[#007bffc0] px-60'>
-            <div className='container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12'>
-                <div className='wf-ull lg:w-1/2 *:text-white'>
-                    <h1>Oops!</h1>
-                    <p className='mt-4'>Sorry, an unexpected error has occurred.</p>
-                    <p className='mt-4'>
-                        Sorry, the page you are looking for doesn't exist. Here are some helpful link:
+        <section className="bg-blue-500 px-4 md:px-20 lg:px-60">
+            <div className="container min-h-screen px-6 py-12 mx-auto flex flex-col lg:flex-row lg:items-center lg:gap-12">
+                {/* Text Content */}
+                <div className="w-full lg:w-1/2 text-white text-center lg:text-left">
+                    <h1 className="text-3xl md:text-5xl font-bold">Oops!</h1>
+                    <p className="mt-4 text-lg">Sorry, an unexpected error has occurred.</p>
+                    <p className="mt-4 text-sm">
+                        Sorry, the page you are looking for doesn't exist. Here are some helpful links:
                     </p>
-
-                    <div className='mt-6'>
+                    <div className="mt-6">
                         <Link
-                            to='/'
-                            className='w-1/2 px-5 py-2 text-sm tracking-wide text-black transition-colors duration-200 bg-white rounded-lg shrink-0 sm:w-auto hover:bg-[#007bffc0] hover:text-white'
+                            to="/"
+                            className="btn border-none text-white text-sm tracking-wide font-semibold bg-blue-700 rounded-lg inline-flex items-center gap-2 hover:bg-blue-600 hover:scale-105 transition-transform"
                         >
-                            Take me home page
+                            <FaArrowLeft /> Take Me Home
                         </Link>
                     </div>
                 </div>
-
-                <div className='relative w-full mt-8 lg:w-1/2 lg:mt-0'>
+                {/* Image Section */}
+                <div className="relative w-full mt-8 lg:w-1/2 lg:mt-0">
                     <img
-                        className=' w-full lg:h-[32rem] h-80 md:h-96 rounded-lg object-cover '
+                        className="w-full lg:h-[32rem] h-80 md:h-96 rounded-lg object-cover"
                         src={error}
-                        alt='Error Page'
+                        alt="Error Page"
                     />
                 </div>
             </div>
